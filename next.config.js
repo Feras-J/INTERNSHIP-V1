2ca93baf32efa81a4bc205268b3ef3dec94b1ada
Feras-J <ferasjabari@hotmail.com>
2025-02-12 22:05:56 +0300

    Revert "```"
    This reverts commit b5b121790e76536aa96cf7dc6114ffd7c179d181.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ module.exports = {
 	// ...existing config...
 	trailingSlash: true,
 	output: 'export',
-	basePath: '/INTERNSHIP-V1',
-	assetPrefix: '/INTERNSHIP-V1/',
 	// ...other config if needed...
 }
