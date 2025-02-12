b5b121790e76536aa96cf7dc6114ffd7c179d181
Feras-J <ferasjabari@hotmail.com>
2025-02-12 19:38:58 +0300

    ``` No code changes detected. ```

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
 	// ...existing config...
 	trailingSlash: true,
 	output: 'export',
+	basePath: '/INTERNSHIP-V1',
+	assetPrefix: '/INTERNSHIP-V1/',
 	// ...other config if needed...
 }
